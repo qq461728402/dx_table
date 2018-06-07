7cddcee63e7c5a9ad638b208763d107558e1206b
qq461728402 <461728402@qq.com>
2018-06-07 14:08:08 +0800

    哈哈

---
+++ src/lib/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dx on 18/6/6.
+ */
+import Slider from './slider.vue'
+const comment = {
+    install: function(Vue) {
+        Vue.component('Slider', Slider)
+    }
+}
+// 这里的判断很重要
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(comment)
+}
+export default comment
